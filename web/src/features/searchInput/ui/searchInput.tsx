872d837f7d50f603ba3b96dbd9d2c80b44fff5f2
Abdulla <abdulla200532@gmail.com>
2025-04-18 18:53:19 +0500

    fix: search input watching search params

--- web/src/features/searchInput/ui/searchInput.tsx
+++ web/src/features/searchInput/ui/searchInput.tsx
@@ -1,25 +1,35 @@
-'use client';
-import { Input } from '@/shared/shadcn/components/ui/input';
-import { SearchIcon } from 'lucide-react';
-import { redirect } from 'next/navigation';
-import React from 'react';
+"use client";
+import { Input } from "@/shared/shadcn/components/ui/input";
+import { SearchIcon } from "lucide-react";
+import { redirect, useSearchParams } from "next/navigation";
+import React, { useEffect } from "react";
 
 const SearchInput = () => {
-  const [search, setSearch] = React.useState('');
+  const params = useSearchParams();
+  const [search, setSearch] = React.useState<string>(getQuery() ?? "");
 
   const handleSearch = () => {
     redirect(`/search?q=${search}`);
   };
 
+  useEffect(() => {
+    const qParam = getQuery();
+    if (qParam && qParam !== search) setSearch(qParam);
+  }, [params]);
+
+  function getQuery(): string | null {
+    return params?.get("q") ?? null;
+  }
+
   return (
-    <div className='relative w-full'>
+    <div className="relative w-full">
       <Input
-        className='w-full border-neutral-300 rounded-full text-lg!'
-        placeholder='Поиск...'
+        className="w-full border-neutral-300 rounded-full text-lg!"
+        placeholder="Поиск..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === 'Enter') {
+          if (e.key === "Enter") {
             e.preventDefault();
             handleSearch();
           }
@@ -29,7 +39,7 @@ const SearchInput = () => {
         size={20}
         strokeWidth={2.5}
         onClick={handleSearch}
-        className='absolute top-1/2 right-4 -translate-y-1/2 stroke-neutral-600 cursor-pointer active:stroke-neutral-950'
+        className="absolute top-1/2 right-4 -translate-y-1/2 stroke-neutral-600 cursor-pointer active:stroke-neutral-950"
       />
     </div>
   );
