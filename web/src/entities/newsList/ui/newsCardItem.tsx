b709ff9487bf051b6fbb16eb100973a9720e07ba
Abdulla <abdulla200532@icloud.com>
2025-04-20 18:53:54 +0500

    refactor: newsCardItem comp refactor link

--- web/src/entities/newsList/ui/newsCardItem.tsx
+++ web/src/entities/newsList/ui/newsCardItem.tsx
@@ -14,6 +14,7 @@ import { format } from 'date-fns';
 import { ru } from 'date-fns/locale';
 import { useRouter } from 'next/navigation';
 import { ROUTES } from '@/app/routes/routes';
+import Link from 'next/link';
 
 type Props = {
   data: ISearchNewsArticleResponse;
@@ -48,10 +49,11 @@ const NewsCardItem = ({
     setIsFavorite(!isFavorite);
   }
 
-  function onClickCard() {
-    onViewed?.(data);
-    router.push(`${ROUTES.NEWS}/${data.id}`);
-  }
+  // TODO: logic in webview
+  // function onClickCard() {
+  //   onViewed?.(data);
+  //   router.push(`${ROUTES.NEWS}/${data.id}`);
+  // }
 
   const date = format(
     new Date(data.publish_date.replace(' ', 'T')),
@@ -60,27 +62,31 @@ const NewsCardItem = ({
   );
 
   return (
-    <Card
-      className='max-w-fit cursor-pointer hover:shadow-lg border hover:border-gray-400 transition-all'
-      onClick={onClickCard}
-    >
+    <Card className='max-w-fit hover:shadow-lg border hover:border-gray-400 transition-all'>
       <CardHeader
         className='relative'
         onMouseEnter={() => setIsFocused(true)}
         onMouseLeave={() => setIsFocused(false)}
       >
         <AspectRatio ratio={16 / 9}>
-          <img
-            src={data.image || noImageSrc}
-            alt={data.title}
-            className='rounded-md h-full w-full object-cover bg-gray-300'
-            loading='lazy'
-            onError={(e) => (e.currentTarget.src = noImageSrc)}
-          />
+          <Link
+            href={`${ROUTES.NEWS}/${data.id}`}
+            onClick={() => onViewed?.(data)}
+            target='_blank'
+            className='cursor-pointer'
+          >
+            <img
+              src={data.image || noImageSrc}
+              alt={data.title}
+              className='rounded-md h-full w-full object-cover bg-gray-300'
+              loading='lazy'
+              onError={(e) => (e.currentTarget.src = noImageSrc)}
+            />
+          </Link>
         </AspectRatio>
         {isFocused && (
           <div
-            className='absolute w-fit p-1 rounded-sm right-1/12 top-2 flex items-center justify-center bg-gray-800/50 backdrop-blur-3xl'
+            className='absolute cursor-pointer w-fit p-1 rounded-sm right-1/12 top-2 flex items-center justify-center bg-gray-800/50 backdrop-blur-3xl'
             onClick={(e) => {
               e.stopPropagation();
               onClickFavorite();
@@ -95,21 +101,30 @@ const NewsCardItem = ({
           </div>
         )}
       </CardHeader>
-      <CardContent className='flex flex-col justify-between h-full gap-2'>
-        <h3 className='font-semibold text-lg text-neutral-950 select-none line-clamp-2 hover:text-indigo-950'>
-          {data.title}
-        </h3>
-        <p className='text-neutral-800 select-none line-clamp-2'>{data.text}</p>
-        <div className='flex justify-between items-center'>
-          <p className='text-sm text-neutral-500'>{date}</p>
-          {isViewed && (
-            <div className='text-sm flex items-center gap-1 text-neutral-500'>
-              <span>Просмотрено</span>
-              <EyeIcon className='size-4' />
-            </div>
-          )}
-        </div>
-      </CardContent>
+      <Link
+        href={`${ROUTES.NEWS}/${data.id}`}
+        onClick={() => onViewed?.(data)}
+        target='_blank'
+        className='cursor-pointer'
+      >
+        <CardContent className='flex flex-col justify-between h-full gap-2'>
+          <h3 className='font-semibold text-lg text-neutral-950 select-none line-clamp-2 hover:text-indigo-950'>
+            {data.title}
+          </h3>
+          <p className='text-neutral-800 select-none line-clamp-2'>
+            {data.text}
+          </p>
+          <div className='flex justify-between items-center'>
+            <p className='text-sm text-neutral-500'>{date}</p>
+            {isViewed && (
+              <div className='text-sm flex items-center gap-1 text-neutral-500'>
+                <span>Просмотрено</span>
+                <EyeIcon className='size-4' />
+              </div>
+            )}
+          </div>
+        </CardContent>
+      </Link>
     </Card>
   );
 };
